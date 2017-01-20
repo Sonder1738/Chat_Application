24bfad699fe382de240f2a69ec8b6c16b2ee017c
Derick <Derick@192.168.0.8>
2017-01-20 09:41:35 +0800

    hy

--- Chat_Application/src/tes.java
+++ Chat_Application/src/tes.java
@@ -44,7 +44,7 @@ public class tes {
 	private JTextField textField;
 
 	/**
-	 * Launch the applicatiiiioooon.
+	 * Launch the applicatiiiilolooooon.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
