5f2c2b1387e9df6b8c13034a6ac8204a533395f2
Sonder1738 <mc_laren_93@hotmail.com>
2017-01-20 08:59:40 +0800

    ha1

--- Chat_Application/src/tes.java
+++ Chat_Application/src/tes.java
@@ -44,7 +44,7 @@ public class tes {
 	private JTextField textField;
 
 	/**
-	 * Launch the applicatioasdasdsss.
+	 * Launch the applicatioaaaaaaaaaaaaaadsss.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
